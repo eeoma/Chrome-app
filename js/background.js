d09c9737668e2b57ee73369027d9ec2f0170d697
eeoma <83899698+eeoma@users.noreply.github.com>
2022-02-03 19:56:07 +0900

    edit_background

--- js/background.js
+++ js/background.js
@@ -10,5 +10,6 @@ const chosenImage=images[Math.floor(Math.random()*images.length)];
 
 const bgImage=document.createElement("img");
 bgImage.src=`img/${chosenImage}`;
+bgImage.className+="bgImg";
 
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage); 
